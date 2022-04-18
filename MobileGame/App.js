16e662a5623b07480109e4daea27edba8445ba9d
Harry DeCecco <beeHSD2720@gmail.com>
2022-04-18 16:19:37 -0700

    Update App.js

--- MobileGame/App.js
+++ MobileGame/App.js
@@ -1,15 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import { ImageBackground, StyleSheet, Text, View, Image } from 'react-native';
+import React from "react";
+import { ImageBackground, StyleSheet, Text, View, Image, TextInput } from 'react-native';
 //View
 export default function App() {
+  const [text, onChangeText] = React.useState("Useless Text");
+  const [number, onChangeNumber] = React.useState(null);
+
+
   return (
     <View style={styles.container}>
       <ImageBackground source={require('./assets/dusk_background.jpg')} resizeMode="cover" style={styles.image}>
-        <View style={styles.view2}>
+        <View style={styles.butterflyView}>
           <Image
           source = {require('./assets/blue_butterfly_image.png')}
           resizeMode="center"
           style = {styles.butterfly}
+          />
+        </View>
+        <View style={styles.loginView}>
+          <TextInput 
+            style={styles.textIn}
+            onChangeText={onChangeNumber}
+            value={number}
+            placeholder= "Username"
+          />
+          <TextInput 
+            style={styles.textIn}
+            placeholder= "Password"
           />
         </View>
       </ImageBackground>
@@ -24,10 +41,18 @@ const styles = StyleSheet.create({
     flex: 1,
   
   },
-  view2: {
+  butterflyView: {
     flex: 1,
     alignItems: 'center',
     paddingTop: 50
+  },
+  loginView: {
+    backgroundColor: 'rgba(163, 163, 163, 0.6)',
+    flex: 1,
+    marginBottom: 25,
+    marginHorizontal: 20,
+    marginTop: 500,
+    borderRadius: 10
   },
   image: {
     flex: 1,
@@ -40,5 +65,13 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
     paddingTop: 100,
   },
+  textIn: {
+    borderBottomColor: '#000000',
+    borderBottomWidth: 1,
+    marginHorizontal: 15,
+    marginVertical: 10,
+    
+  },
+  
 
 });
