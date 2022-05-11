2b1453973e23545c8a750eb4cea78d04955fee58
BaileyMcCauslin <66763821+BaileyMcCauslin@users.noreply.github.com>
2022-05-11 13:49:40 -0700

    Added PracticeScreen

---
+++ MobileGame/screens/PracticeScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View , Text } from 'react-native';
+
+export default function PracticeScreen( ){
+    return(
+        <View>
+            <Text>
+                PracticeScreen
+            </Text>
+        </View>
+    );
+}
