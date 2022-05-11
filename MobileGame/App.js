c0a9a16bbfb43dc65868bc1b2c82e546eafd9de2
BaileyMcCauslin <66763821+BaileyMcCauslin@users.noreply.github.com>
2022-05-11 13:45:32 -0700

    Added stack routes to screens

--- MobileGame/App.js
+++ MobileGame/App.js
@@ -4,6 +4,9 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Provider as PaperProvider } from 'react-native-paper';
 import LoginScreen from './screens/LoginScreen';
 import WellnessScreen from './screens/WellnessScreen';
+import HomeScreen from './screens/HomeScreen';
+import PracticeScreen from './screens/PracticeScreen';
+import LearnScreen from './screens/LearnScreen';
 
 const Stack = createNativeStackNavigator()
 
@@ -17,6 +20,9 @@ export default function App() {
         }}>
           <Stack.Screen name="Login" component={LoginScreen} />
           <Stack.Screen name="Wellness" component={WellnessScreen} />
+          <Stack.Screen name="Home" component={ HomeScreen } />
+          <Stack.Screen name="Practice" component={ PracticeScreen } />
+          <Stack.Screen name="Learn" component={ LearnScreen } />
         </Stack.Navigator>
     </NavigationContainer>
     </PaperProvider>
