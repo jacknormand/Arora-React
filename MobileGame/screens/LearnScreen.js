111b37ce28d07e52114e514d3d5c8b44eb02967d
BaileyMcCauslin <66763821+BaileyMcCauslin@users.noreply.github.com>
2022-05-11 13:48:39 -0700

    Added LearnScreen

---
+++ MobileGame/screens/LearnScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View , Text } from 'react-native';
+
+export default function LearnScreen( ){
+  return(
+    <View>
+      <Text>
+          Learn Screen
+      </Text>
+    </View>
+  );
+}
