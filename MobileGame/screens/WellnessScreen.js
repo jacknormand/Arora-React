e4aae477c180172d88509c709080f820ead41860
jacknormand <jack.normand15@gmail.com>
2022-05-16 11:30:38 -0700

    Sliders
    added sliders to wellness screen and fixed up some stuff

--- MobileGame/screens/WellnessScreen.js
+++ MobileGame/screens/WellnessScreen.js
@@ -1,18 +1,38 @@
 import React from 'react';
 import { StyleSheet, View , TouchableOpacity , Text , ImageBackground , Image } from 'react-native';
 import { Card } from 'react-native-paper';
-import { DefaultTheme } from 'react-native-paper';
+import Slider from '@react-native-community/slider';
 
 function WellnessScreen ({ navigation }) {
+  
   return (
     <View style={styles.container}>
      <ImageBackground source={require('../assets/dusk_background.jpg')} resizeMode="cover" style={ styles.backgroundImage }>
+
       <Card style={styles.card}>
         <Text style={ styles.cardText }>How is your mood today?</Text>
+        <Image source={require('../assets/surveryScreen/black_butterfly.png')} style={ styles.butterfly }/>
+        <Slider
+        style={ styles.slider }
+        minimumValue={0}
+        maximumValue={100}
+        minimumTrackTintColor="#ff0045"
+        maximumTrackTintColor="#000000"
+      />
       </Card>
+
       <Card style={styles.cardTwo}>
         <Text style={ styles.cardText }>How stressed are you today?</Text>
+        <Image source={require('../assets/surveryScreen/black_butterfly.png')} style={ styles.butterfly }/>
+        <Slider
+        style={ styles.slider }
+        minimumValue={0}
+        maximumValue={100}
+        minimumTrackTintColor="#ff0045"
+        maximumTrackTintColor="#000000"
+      />
       </Card>
+
       <View style={ styles.textAlign }>
        <TouchableOpacity style={ styles.button } onPress={() => navigation.navigate('Home')}>
         <Text style={ styles.buttonText }> SKIP </Text>
@@ -34,6 +54,7 @@ const styles = StyleSheet.create({
   cardText:{
     color: 'white',
     fontSize: 30,
+    textAlign: 'center',
   },
   
   textAlign:{
@@ -44,6 +65,15 @@ const styles = StyleSheet.create({
   
   backgroundImage:{
     flex: 1,
+  },
+
+  butterfly:{
+    alignSelf: 'center',
+  },
+
+  slider:{
+    alignSelf: 'center',
+    width: 250,
   },
   
   button:{
@@ -73,7 +103,7 @@ const styles = StyleSheet.create({
     backgroundColor: 'rgba(163, 163, 163, 0.2)',
     marginBottom: 5,
     marginHorizontal: 20,
-    marginTop: 30,
+    marginTop: 40,
     width: '85%',
     height: '40%',
     alignItems: 'center',
@@ -88,6 +118,16 @@ const styles = StyleSheet.create({
     height: '40%',
     alignItems: 'center',   
   },
+
+
+
+
+
+
+
+
+
+  
 });
 
 export default WellnessScreen;
