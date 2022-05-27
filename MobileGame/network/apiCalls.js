a3879bbb00e7782a22975cb492fb661d18f21a49
BaileyMcCauslin <baileymccauslin@gmail.com>
2022-05-27 12:02:13 -0700

    Just some comments for future reference

--- MobileGame/network/apiCalls.js
+++ MobileGame/network/apiCalls.js
@@ -3,6 +3,7 @@ import { Alert } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage'
 
 //Api ip routes
+const regIp = 'http://104.248.178.78:8000/';
 const loginApiIp = 'http://104.248.178.78:8000/api-token-auth';
 const moodFormApiIp = 'http://104.248.178.78:8000/moodreport';
 
@@ -23,23 +24,23 @@ export async function updateDatabase(){
    //let b2 = await AsyncStorage.getItem( '@user_b2_count' );
    //let b3 = await AsyncStorage.getItem( '@user_b3_count' );
    //let b4 = await AsyncStorage.getItem( '@user_b4_count' );
-   //let currentButterfly = await AsyncStorage.getItem( '@current_butterfly' );
+   let currentButterfly = await AsyncStorage.getItem( '@current_butterfly' );
    let userMood = await AsyncStorage.getItem( '@user_current_mood' );
-   let timeSubmmited = await AsyncStorage.getItem( '@user_current_mood_updated' );
+   //let timeSubmmited = await AsyncStorage.getItem( '@user_current_mood_updated' );
    await fetch('http://104.248.178.78:8000/userinfo/' + userId , {
     method: 'PATCH',
     headers:{
     'Content-Type':'application/json'
     },
-    body: JSON.stringify({
+    body: JSON.stringify({ // NEED TO CHECK IF WE STILL NEED THESE VALUES FOR UPDATE
 	   "user_current_mood": userMood,
-	   "user_current_mood_updated": "2019-02-23T09:38:42.925706Z",
+	   "user_current_mood_updated": "2019-02-23T09:38:42.925706Z", //See if we still need to update location data
 	   "user_current_location_updated":"2019-02-23T09:38:42.925706Z",
-	   "user_current_butterfly": 1,
+	   "user_current_butterfly": currentButterfly,
 	   "user_current_location_lat": 0.7,
 	   "user_current_location_long": 0.2,
 	   "user_pollen": userPollen,
-	   "user_points": 3
+	   "user_points": 3 // What is this
     })
    }).then( response => {
      return response.json();
@@ -49,6 +50,52 @@ export async function updateDatabase(){
    })
 }
 
+//Function to create user butterfly( NOT DONE ) 
+/*
+MIGHT NOT NEED THESE BUT GOING TO CHECK ATTRIUM DATA 
+==========================================================
+export async function updateButterFlies(){
+  let userId = await AsyncStorage.getItem('@userId');
+  await fetch( regIp + 'userbutterfly' , {
+    method: 'POST',
+    headers:{
+    'Content-Type':'application/json' 
+    },
+    body: JSON.stringify({
+      "butterfly_id":2,
+      "user_id": userId
+    })
+  }).then( response => {
+    return response.json();
+  }).catch( error => {
+    console.error( error );
+  })
+}
+
+//Function to create a user attrium for first time( NOT DONE )
+export async function createAttrium(){
+  let userId = await AsyncStorage.getItem( '@userId' );
+  //Not real URL
+  await fetch( 'http://104.248.178.78:8000/ButterflyAtriums/butterflyatrium' , {
+   method: 'POST',
+   headers:{
+   'Content-Type':'application/json' 
+   },
+   body: JSON.stringify({
+    'user_id' : userId,
+    'user_b0_count' : 0,
+    'user_b1_count' : 0,
+    'user_b2_count' : 0,
+    'user_b3_count' : 0,
+  })
+ }).then( response => {
+   return response.json();
+ }).catch( error => {
+   console.log( error );
+ })
+}
+===================================================================
+*/
 
 export async function storeUserData(){
   //get current user id and fetch the user data
@@ -153,10 +200,12 @@ export async function moodReportAPI( navigation ){
     var currentdate = new Date(); 
     let timeSubmmited = currentdate.getFullYear() + "-"
                        + (currentdate.getMonth()+1)  + "-" 
-                       + currentdate.getDate() + "@"
+                       + currentdate.getDate() + "TO"
                        + currentdate.getHours() + ":"  
                        + currentdate.getMinutes() + ":" 
-                       + currentdate.getSeconds()
+                       + currentdate.getSeconds() + "."
+                       +currentdate.getMilliseconds()
+    console.log( timeSubmmited );
     AsyncStorage.setItem('@user_current_mood_updated' , JSON.stringify( timeSubmmited) );
     await fetch( moodFormApiIp, {
         method: 'POST',
