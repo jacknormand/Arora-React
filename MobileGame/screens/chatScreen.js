cfeda8ac9360944cc9388e2f72f23ad1af91a573
BaileyMcCauslin <baileymccauslin@gmail.com>
2022-06-08 12:52:27 -0700

    Added base template for the chat

--- MobileGame/screens/chatScreen.js
+++ MobileGame/screens/chatScreen.js
@@ -1,14 +1,28 @@
-import React from 'react'
-import { View , Text } from 'react-native'
+import React , { useEffect , useCallback } from 'react'
 import { GiftedChat } from 'react-native-gifted-chat'
+import { Alert } from 'react-native';
 
 export default function ChatScreen(){
+    const [ messages , setMessages ] = React.useState( [] );
+    
+    //Create the message object
 
+    //User object maybe? 
+    
+      
+    //append the sent message to the message array 
+    const onSend = useCallback(( messages = [] ) => {
+        setMessages( previousMessages => GiftedChat.append( previousMessages, messages ) )
+    }, [] )
     
     return(
-        <View>
-            
-            <Text> Chat Screen </Text>
-        </View>
+       <GiftedChat
+       messages={ messages }
+       isTyping={ true }
+       onSend={ messages => onSend( messages ) }
+       user={{
+         _id: 1,
+       }} 
+       />
     )
 }
